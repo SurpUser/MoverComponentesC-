f3dbe1b46275bf6cd4bb5bdd568cdf3505546d79
surpuser <francisjcv@hotmail.com>
2019-03-09 10:16:00 -0400

    Agregar archivos de proyecto.

---
+++ EjemploMoverObjetos/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace EjemploMoverObjetos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::EjemploMoverObjetos.Properties.Resources.Mesa1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(270, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 73);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 490);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EjemploMoverObjetos/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploMoverObjetos
+{
+    public partial class Form1 : Form
+    {
+        int LocationX = 0, LocationY = 0;
+        int LocationFormX = 0, LocationFormY = 0;
+        int LocationActualX = 0, LocationActualY = 0;
+        bool Precionado = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LocationActualX = button1.Location.X;
+            LocationActualY = button1.Location.Y;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            LocationX = e.Location.X;
+            LocationY = e.Location.Y;
+            Precionado = true;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Precionado = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            LocationFormX = e.Location.X;
+            LocationFormY = e.Location.Y;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            LocationFormX = LocationActualX + e.Location.X;
+            LocationFormY= LocationActualY + e.Location.Y;
+            if (Precionado)
+            {
+                MoverBoton();
+            }
+        } 
+
+        private void MoverBoton()
+        {
+            button1.Location = new System.Drawing.Point(LocationFormX - LocationX,LocationFormY - LocationY);
+            LocationActualX = button1.Location.X;
+            LocationActualY = button1.Location.Y;
+        }
+
+    }
+}
